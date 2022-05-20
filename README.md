031031579aeae2d85a56b28dd8355246146995a0
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2022-05-20 17:35:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# quran_ocr_backend_server
+
+Automatically determine Quran word meaning through a mobile phone's camera. 
+This is the backend server to support the mobile app. 
+
+Here are the tasks done by the server:
+
+1) Receive the image uploaded from the mobile
+2) Perform OCR to determine the text in it
+3) Return the results back
+
+Current version: v1.0
+
+Uses Flask. Command to run:
+FLASK_APP=./src/quran_ocr_.py flask run --host=0.0.0.0
+
+[Click Here for a video demo](https://youtube.com/shorts/DCRXBbBdsLg)
+
+<img src="https://raw.githubusercontent.com/hashirabdulbasheer/my_assets/master/quran_ocr.png" width="25%">
+
+
+
+
+
---
+++ src/quran_ocr_.py
@@ -0,0 +1,49 @@
+from ArabicOcr import arabicocr
+from flask import Flask, request, Response, jsonify
+import os
+import time
+import json
+
+UPLOAD_FOLDER = './'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = ""
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def hello_world():
+    return 'Hello'
+
+@app.route('/ocr', methods=['POST'])
+def run_ocr():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+		    return "File not found"
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+			# get the file input
+			millisec = time.time() * 1000
+			filename = 'sample_' + str(millisec) + '.jpg'
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			out_image = 'out_' + str(millisec) + '.jpg'
+			# perform OCR
+			results = arabicocr.arabic_ocr(filename, out_image)
+			# format response
+			words=[]
+			for i in range(len(results)):
+				word=results[i][1]
+				if word:
+					words.append(word)
+			# cleanup					
+			os.remove(filename)
+			os.remove(out_image)
+			# return
+			return json.dumps(words)
+		return "Error"			
+
